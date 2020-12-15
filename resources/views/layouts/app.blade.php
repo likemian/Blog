1524f7715a6b44294c86bd2b5c841a16ae99f218
likemian <18624269120@163.com>
2020-12-15 20:39:14 +0800

    add title and title icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- title name and icon setting -->
     <title>Blog</title>
+    <link rel="icon" href="images/title.ico" type="image/x-icon" />
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
