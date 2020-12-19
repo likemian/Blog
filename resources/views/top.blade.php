610368f8e34eb474a49d5648a877990e9d1cc5cc
likemian <18624269120@163.com>
2020-12-19 16:41:42 +0800

    Blog system top page

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -36,28 +36,29 @@
                 <h1>才疏学浅</h2>
                 <p id="top-subtitle">Sun, Moon, You</p>
             </div>
-            <div class="menu">
+            <div class="follow-me">
+                <div class="contact">
+                    <ul>
+                        <li>
+                            <a href="mailto: 18624269120@163.com">
+                                <i class="ion-ios-mail"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/likemian/">
+                                <i class="ion ion-logo-github"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="menu-contact">
                 <a class="nav-menu" href="blog">博客</a>
                 <a class="nav-menu" href="life">随笔</a>
                 <a class="nav-menu" href="xmind">思维导图</a>
                 <a class="nav-menu" href="about">关于</a>
-            </div>
-        </div>
-        <div class="follow-me">
-            <span class="social-title">Follow Me |</span>
-            <div class="contact">
-                <ul>
-                    <li>
-                        <a href="mailto: 18624269120@163.com">
-                            <i class="ion-ios-mail"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/likemian/">
-                            <i class="ion ion-logo-github"></i>
-                        </a>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
